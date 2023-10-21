c8138e0084ba881c04794cb9efe70f89948135cb
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-10-21 07:28:16 -0300

    Create FilterJavascript.js

---
+++ FilterJavascript.js
@@ -0,0 +1,12 @@
+var nomes = ["joao", "vitor", "talilo" , "paulo","maria"]
+
+var resu = nomes.filter(nome=>nome !=="joao")
+var nomes = ["joao", "vitor", "talilo" , "paulo","maria"]
+
+console.log(resu);
+
+// https://www.alura.com.br/artigos/conhecendo-arrow-functions?utm_term=&utm_campaign=%5BSearch%5D+%5BPerformance%5D+-+Dynamic+Search+Ads+-+Artigos+e+Conteúdos&utm_source=adwords&utm_medium=ppc&hsa_acc=7964138385&hsa_cam=11384329873&hsa_grp=111087461203&hsa_ad=662261158752&hsa_src=g&hsa_tgt=aud-539280195484:dsa-843358956400&hsa_kw=&hsa_mt=&hsa_net=adwords&hsa_ver=3&gclid=EAIaIQobChMIn9vFh82WgQMVClF_AB2qNwaWEAAYASAAEgJRGPD_BwE
+var nomes = ["joao", "vitor", "talilo" , "paulo","maria"]
+
+
+// https://www.alura.com.br/artigos/manipulacao-de-array-com-map-filter-e-reduce?utm_term=&utm_campaign=%5BSearch%5D+%5BPerformance%5D+-+Dynamic+Search+Ads+-+Artigos+e+Conteúdos&utm_source=adwords&utm_medium=ppc&hsa_acc=7964138385&hsa_cam=11384329873&hsa_grp=111087461203&hsa_ad=662261158752&hsa_src=g&hsa_tgt=aud-539280195004:dsa-843358956400&hsa_kw=&hsa_mt=&hsa_net=adwords&hsa_ver=3&gclid=EAIaIQobChMIjPaLicqWgQMVejLUAR2eOQ17EAAYASAAEgLOw_D_BwE
