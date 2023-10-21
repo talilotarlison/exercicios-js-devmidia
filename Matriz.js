a033e9e633438d3d0a1946496feaa7e169750603
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-10-21 07:48:43 -0300

    Create Matriz.js

---
+++ Matriz.js
@@ -0,0 +1,26 @@
+// if com uma so linha não precisa de chave
+// https://editor.p5js.org/
+num = 27
+
+console.log("numero de verificao deve ser igual a 23 -",num)
+
+if( num == 23) console.log("igual")
+
+else console.log("diferente")
+
+
+//array 
+
+var num2 = [11,12,24]
+
+console.log(num2[2])
+
+// matriz com array com coluna e linhas
+
+var num = [
+  ["black", 132 , 73223200 ],
+  ["gray", 132 , 703230 ],
+  ["blue", 123 , 703230 ] 
+]
+
+console.log(num[2][2])
