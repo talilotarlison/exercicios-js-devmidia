4418bf497f55a8e9b1418c7c962d4cfcd8fed248
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-10-21 07:47:22 -0300

    Create OperadorDeCoalescencia.js

---
+++ OperadorDeCoalescencia.js
@@ -0,0 +1,31 @@
+
+var turma = {
+  
+  aluno : "talilo",
+  idade : 28,
+  curso: {
+    ano:2019,
+    id : 263738
+  }
+}
+
+//https://www.freecodecamp.org/portuguese/news/como-funciona-o-operador-de-ponto-de-interrogacao-em-javascript/
+
+//encadeamento opcional
+
+var resu = turma?.curso.ano;
+
+console.log(resu);
+
+//operador de coalescencia
+
+var nome = undefined || "jose"
+
+var number = null ?? 88
+
+var verificar = false ?? "consulta"
+
+
+console.log(nome)
+console.log(number)
+console.log(verificar)
