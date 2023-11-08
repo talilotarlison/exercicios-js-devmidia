f2d99894bdd9487e3d4a03e4c763e99c20ccaa45
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-11-08 14:23:37 -0300

    Create Função IIFE.js

---
+++ Função IIFE.js
@@ -0,0 +1,9 @@
+/*A sintaxe ()() em JavaScript é usada para chamar uma função imediatamente após a definição dela. Isso é comumente conhecido como uma função imediatamente invocada (IIFE, do inglês "Immediately Invoked Function Expression"). Um exemplo de uso seria:*/
+
+javascript
+Copy code
+(function() {
+    // Código da função aqui
+  console.log("funcao invocada instantâneamente");
+})();
+/*Isso cria uma função anônima e a chama imediatamente. É útil para criar escopos isolados e evitar conflitos de variáveis globais.*/
