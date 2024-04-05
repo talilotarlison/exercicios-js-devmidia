c3a7134ee7c1180f2e9613017e90eaf7d717510b
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-04-05 09:43:28 -0300

    modificações

--- callback.js
+++ callback.js
@@ -16,8 +16,9 @@ let altenticaUser = {
 
 altenticaUser.login(usuario);
 
-// função de callback em JS
+// função de callback em JS:
 // https://developer.mozilla.org/pt-BR/docs/Glossary/Callback_function
+
 function greeting(name) {
   console.log("Olá " + name);
 };
@@ -29,6 +30,6 @@ function processUserInput(callback, name) {
 
 processUserInput(greeting, 'Meu nome é Talilo!');
 
-// estou de pilha 
+// estouro de pilha: 
 
 //https://codelogica.wordpress.com/2018/12/25/estouro-de-pilha/
