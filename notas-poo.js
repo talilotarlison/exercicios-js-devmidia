e29485fb1daee4f9bca9a0e97a5279c1d26d58bb
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-07-22 01:36:55 -0300

    Create notas-poo.js

---
+++ notas-poo.js
@@ -0,0 +1,43 @@
+// https://www.w3schools.com/Js/js_classes.asp
+
+class Curso {
+  nome;
+  disciplina;
+  matricula; 
+  constructor(nome, disciplina,matricula) {
+    this.nome = nome;
+    this.disciplina = disciplina;
+    this.matricula = matricula;
+  }
+  
+  statusAprovacaoAluno(notas){
+    let aluno = new Notas(notas);
+    let mediaNotas = aluno.calculaMedia();
+    var status = mediaNotas >=6 ? "Aprovado": "Reprovado"
+    console.log("Nota: ", mediaNotas," - ", status)
+  }
+
+}
+
+
+class Notas {
+  notas= [];
+  constructor(notas) {
+    this.notas= notas;
+
+  }
+  
+  calculaMedia(){
+    var soma = 0;
+    for(var i = 0; i < this.notas.length; i++){
+      soma += this.notas[i];
+    }
+    return (soma/this.notas.length);
+  }
+
+}
+
+
+let aluno = new Curso("Joao", "Matematica", 214214)
+
+aluno.statusAprovacaoAluno([6,6,6])
